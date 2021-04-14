08112001d07a24cbc999936738c6d82332b32d90
miguel <usmo300@hotmail.com>
2021-04-14 11:00:15 +0200

    ultimos cambios subidos

--- sForm2.js
+++ sForm2.js
@@ -1,10 +1,4 @@
 let form = document.getElementById("sForm");
-let form = document.getElementById("sForm");
-let form = document.getElementById("sForm");
-let form = document.getElementById("sForm");
-let form = document.getElementById("sForm");
-let form = document.getElementById("sForm");
-let form = document.getElementById("sForm");
 
 form.addEventListener("submit", (event)=>{
     event.preventDefault();
@@ -16,28 +10,55 @@ form.addEventListener("submit", (event)=>{
 });
 
 function runAjax(){
-
+    
     let xhr;
-
+    
     if(window.XMLHttpRequest){
         xhr = new XMLHttpRequest();
     }else{
         xhr= new ActiveXObject("Microsoft.XMLHTTP");
     }
-
+    
     xhr.open("POST", "controller.php");
     
     let formData = new FormData(form);
-
+    
     formData.append("creatorForm", "Miguel")
-
+    
     xhr.send(formData);
 }
 
 function validaForm() {
     let formularioCorrecto = true;
 
+    let name = document.getElementById("name");
+    if( name == null || name.length == 0 || /^\s+$/.test(name) ) {
+        alert("El campo de nombre no está correctamente rellenado")
+        return formularioCorrecto = false;
+    }
 
+    let surname = document.getElementById("surname");
+    if( surname == null || surname.length == 0 || /^\s+$/.test(surname) ) {
+        alert("El campo de apellido no está correctamente rellenado")
+        return formularioCorrecto = false;
+    }
+    
 
+    let age = document.getElementById("age");
+    if (age.length == 0){
+        alert('Porfavor, indique su edad.');
+        return;
+    }else if(age < 18){
+        alert("No se puede regirtrar a personas menores de 18 años.");
+        return 0;
+    }
+
+    let dniNumber = document.getElementById("dniNumber");
+    
+    let email = document.getElementById("email");
+    
+    let description = document.getElementById("description");
+    
+    
     return formularioCorrecto;
 }
